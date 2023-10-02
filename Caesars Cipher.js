f89a32b62c65f6802dc8556efe8f58332ed64b8f
The-Aldi-Tri <the.aldi.tri@gmail.com>
2023-10-03 06:42:47 +0700

    Improve code style

--- Caesars Cipher.js
+++ Caesars Cipher.js
@@ -1,18 +1,19 @@
-function rot13(str) {
-    let letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    letter = letter.split("");
-    let decode = [];
+function rot13 (str) {
+    const letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const arrLetter = letter.split('')
+    const decoded = []
   
-    for(let i=0; i<str.length; i++){
-      if(letter.indexOf(str[i]) == -1){
-        decode.push(str[i]);
-      } else{
-        let idx = (letter.indexOf(str[i])+13)%26
-        decode.push(letter[idx]);
+    for (let i = 0; i < str.length; i++) {
+      if (arrLetter.indexOf(str[i]) === -1) {
+        decoded.push(str[i])
+      } else {
+        const idx = (arrLetter.indexOf(str[i]) + 13) % 26
+        decoded.push(arrLetter[idx])
       }
     }
-    
-    return decode.join("");
+  
+    return decoded.join('')
   }
   
-  rot13("SERR PBQR PNZC");
+  rot13('SERR PBQR PNZC')
+  

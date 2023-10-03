cb3933b0bb8951309950af3f0f975c68b54508d4
The-Aldi-Tri <the.aldi.tri@gmail.com>
2023-10-03 11:16:20 +0700

    Improve code performance and readibility

--- Cash Register.js
+++ Cash Register.js
@@ -1,41 +1,51 @@
 function checkCashRegister (price, cash, cid) {
-    let change = cash - price
-    const n = cid.length
-    const unit = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
-    const subtotal = cid.map(item => item[1])
-    const arr = []
-    let i = n - 1
-    while (i >= 0 && change > 0) {
-      if (change >= unit[i] && change >= subtotal[i] && Number(subtotal[i]) !== 0) {
-        arr.push(cid[i])
-        change = change - subtotal[i]
-        change = Math.round((change + Number.EPSILON) * 100) / 100
-        subtotal[i] = 0
-        i--
-      } else if (change >= unit[i] && change < subtotal[i] && Number(subtotal[i]) !== 0) {
-        arr.push([cid[i][0], Math.floor(change / unit[i]) * unit[i]])
-        change = change % unit[i]
-        change = Math.round((change + Number.EPSILON) * 100) / 100
-        subtotal[i] = subtotal[i] - Math.floor(change / unit[i]) * unit[i]
-        i--
-      } else {
-        arr.push([cid[i][0], 0])
-        i--
+    let changeTotal = cash - price
+    const cidTotal = cid.reduce((acc, curr) => acc + curr[1], 0)
+    if (changeTotal > cidTotal) return { status: 'INSUFFICIENT_FUNDS', change: [] }
+    if (changeTotal === cidTotal) return { status: 'CLOSED', change: cid }
+  
+    const cidObj = cid.reduce((acc, curr) => {
+      const key = curr[0]
+      const value = curr[1]
+      acc[key] = value
+      return acc
+    }, {}
+    )
+  
+    const currencyUnitAndAmount = {
+      'ONE HUNDRED': 100.00,
+      TWENTY: 20.00,
+      TEN: 10.00,
+      FIVE: 5.00,
+      ONE: 1.00,
+      QUARTER: 0.25,
+      DIME: 0.10,
+      NICKEL: 0.05,
+      PENNY: 0.01
+    }
+  
+    const change = []
+    for (const key in currencyUnitAndAmount) {
+      if (changeTotal >= currencyUnitAndAmount[key] && changeTotal >= cidObj[key]) {
+        changeTotal -= cidObj[key]
+        changeTotal = Math.round((changeTotal) * 100) / 100
+        change.push([key, Math.round((cidObj[key]) * 100) / 100])
+      } else if (changeTotal >= currencyUnitAndAmount[key] && changeTotal < cidObj[key]) {
+        const changeTotalBefore = changeTotal
+        changeTotal %= currencyUnitAndAmount[key]
+        changeTotal = Math.round((changeTotal) * 100) / 100
+        change.push([key, Math.round(((changeTotalBefore - changeTotal)) * 100) / 100])
       }
     }
   
-    const total = subtotal.reduce((a, b) => a + b, 0)
-    if (arr.length === 0 || change > 0) {
-      return { status: 'INSUFFICIENT_FUNDS', change: [] }
-    } else if (Number(total) === 0) {
-      const newArr = []
-      for (const e in arr) {
-        newArr.push(arr[arr.length - e - 1])
-      }
-      return { status: 'CLOSED', change: newArr }
-    } else {
-      return { status: 'OPEN', change: arr.filter(item => Number(item[1]) !== 0) }
-    }
+    if (changeTotal > 0) return { status: 'INSUFFICIENT_FUNDS', change: [] }
+    // If you need to update cid(cash-in-drawer) //
+    // const updatedCidObj = cidObj
+    // for (let i = 0; i < change.length; i++) {
+    //   updatedCidObj[change[i][0]] = Math.round((updatedCidObj[change[i][0]] - change[i][1]) * 100) / 100
+    // }
+    // const updatedCid = Object.entries(updatedCidObj);
+    return { status: 'OPEN', change }
   }
   
   console.log(checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))
